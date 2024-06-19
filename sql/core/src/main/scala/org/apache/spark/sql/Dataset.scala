a961f0305bfe1ba4a434ebe52acaceb7444c9f41
Xinrong Meng <xinrong@apache.org>
2024-06-18 17:40:01 -0700

    fix firstColumnValues

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -2174,9 +2174,9 @@ class Dataset[T] private[sql](
     unpivot(ids.toArray, variableColumnName, valueColumnName)
 
 
-  def transpose(firstColumnValues: Seq[Column]): DataFrame = withPlan {
+  def transpose(firstColumnValues: Seq[ColumnName]): DataFrame = withPlan {
     Transpose(
-      firstColumnValues.map(_.expr),
+      firstColumnValues.map(col => Literal(col.toString())),
       logicalPlan
     )
   }
